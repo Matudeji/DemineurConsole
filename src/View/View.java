e2f720637abe45eca5c2cbd9a4dc327810de4343
ArthurTlprt <arthur.talpaert@isen-lille.fr>
2017-03-15 11:28:03 +0100

    création du package view, de l'interface view, et de l'implémentation de ViewConsole

---
+++ src/View/View.java
@@ -0,0 +1,19 @@
+package View;
+
+
+import Model.Map;
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author arthur
+ */
+public interface View {
+    public void print();
+}
