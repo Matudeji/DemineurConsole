2483bf1d67e99972e05f7911ea40d7127b9e09a3
ArthurTlprt <arthur.talpaert@isen-lille.fr>
2017-03-29 11:09:38 +0200

    dernier commit avant de mourir

---
+++ src/demineurgraphique/Main.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demineurgraphique;
+
+import Controller.Controller;
+import Model.Game;
+import Model.Point;
+import View.ViewGUI;
+import java.awt.GridLayout;
+import java.util.Scanner;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author arthur
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Game game = new Game();
+
+        game.createMap(10, 10, 10, game);
+        ViewGUI viewG = new ViewGUI(game, game);
+        //game.addObserver(viewG);
+        //game.addView(viewG);
+
+        //JFrame frame = new JFrame("lol");
+        
+
+        //game.initMines(new Point(2, 2));
+    }
+}
